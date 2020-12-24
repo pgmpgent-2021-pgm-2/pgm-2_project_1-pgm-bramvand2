a7d150b71031b219838a9a9130aaf497cd2df2e4
«pgm-bramvand2» <bramvand2@student-arteveldehs.xn--be-efa>
2020-12-24 18:47:01 +0100

    added majority of javascript

--- static/js/main.js
+++ static/js/main.js
@@ -5,46 +5,186 @@
                 this.cacheElements();
                 this.fetchWeatherData();
                 this.fetchCovidData();
-                this.searchUsers();
+                this.fetchUsersData();
+                this.updateGitHubUsersContainer();
             },
 
             cacheElements () {  
-                this.$weatherDisplay = document.querySelector('#weather-display');
-                this.$covidDisplayCases = document.querySelector('#covid-display__cases');
+                this.$weatherContainer = document.querySelector('#weather__container');
+                this.$covidCasesContainer = document.querySelector('#covid-cases__container');
                 this.$searchBar = document.querySelector('#search-bar');
-                this.$searchButton = document.querySelector('#search-button')
+                this.$usersContainer = document.querySelector('#users__container');
+                this.$userDetail = document.querySelector('#user__detail');
+                this.$userReposContainer = document.querySelector('#repositories__container');
+                this.$userFollowersContainer = document.querySelector('#followers__container');
+                this.$gitHubUsersContainer =  document.querySelector('#github-users__container')
             },
 
             async fetchWeatherData () {
                const weatherApi = new WeatherApi();
                const weatherData = await weatherApi.getWeatherData();
-               this.updateWeatherDisplay(weatherData)
+               this.updateWeatherContainer(weatherData)
             },
 
-            updateWeatherDisplay (weatherData) {
-                const weatherDisplay =  `
+            updateWeatherContainer (weatherData) {
+                const weatherContainer =  `
                                         <li>${weatherData.location.name}</li>
                                         <li>${weatherData.current.temp_c} °</li>
                                         <li><img src="${weatherData.current.condition.icon}" alt=" ${weatherData.current.condition.text}"></li>
                                         `;
-                this.$weatherDisplay.innerHTML = weatherDisplay;
+                this.$weatherContainer.innerHTML = weatherContainer;
             },
 
             async fetchCovidData () {
                 const covidApi = new CovidApi();
                 const covidData = await covidApi.getCovidData();
-                this.updateCovidDisplay(covidData);
+                this.updateCovidContainer(covidData);
             },
 
-            updateCovidDisplay(covidData) {
-                console.log(covidData.records[0].fields.cases)
-                this.$covidDisplayCases.innerHTML = `${covidData.records[0].fields.cases}`;
+            updateCovidContainer(data) {
+                console.log(data.records[0].fields.cases)
+                this.$covidCasesContainer.innerHTML = `${data.records[0].fields.cases}`;
             },
 
-            searchUsers () {
-                this.$searchBar.addEventListener('input', () => {
+            async fetchUsersData () {
+                const usersApi = new UsersApi();
+                const usersData = await usersApi.getUserData();
+                this.updateUserContainer(usersData);
+            },
+
+            updateUserContainer(data){
+                const sortedUsers = data.users.sort((user1, user2) => {
+                    return user1.lastName.localeCompare(user2.lastName); 
+                });
+
+                const updatedUserContainer = sortedUsers.map((user) => {
+                    console.log(user.gitHubUserName)
+                    return `
+                            <li id="${user.portfolio.gitHubUserName}">
+                                <img src="${user.thumbnail}" alt="Photo of ${user.firstName} ${user.lastName}">
+                                <span>${user.firstName} ${user.lastName}</span>
+                            </li>
+                                `;
+                        });
+                                       
+                this.$usersContainer.innerHTML = updatedUserContainer;
+
+                sortedUsers.forEach(user => {
+                    const $user = document.querySelector(`#${user.portfolio.gitHubUserName}`);
+                    console.log($user)
+                    $user.addEventListener('click', () => {
+                        this.updateUserDetail(user);
+                        this.updateUserRepos(user.portfolio.gitHubUserName);
+                        this.updateUserFollowers(user.portfolio.gitHubUserName);
+                    } )
+                });               
+            },            
+
+            updateUserDetail (user) {                
+                const updatedUserDetail = `
+                                <img src="${user.thumbnail}" alt="Photo of ${user.firstName} ${user.lastName}">
+                                <h2>${user.firstName} ${user.lastName}</h2>
+                                <p>${user.motto}</p> 
+                                <span>${user.lecturer ? 'Lecturer' : 'Student'}</span>
+                                <span>Age: ${this.calculateAge(user.dateOfBirth)}`;
+                this.$userDetail.innerHTML = updatedUserDetail;
+            },
+
+            calculateAge (userAge) {
+                const newDate =  new Date();
+                const currentYear = newDate.getFullYear();
+                const userDate = new Date(userAge);
+                const yearOfBirth  = userDate.getFullYear();
+                return currentYear - yearOfBirth;                
+            },
+
+            async fetchUserRepos(user) {
+                const userReposApi = await new GitHubApi();
+                const userReposData = await userReposApi.getUserRepos(user);
+                return userReposData;
+            },
+
+            async updateUserRepos (user) {
+                const userReposData = await this.fetchUserRepos(user);
+                console.log(userReposData)
+                const userRepos = await userReposData.map((repo) => {
+                    console.log(repo)
+                    return `<li>${repo.name}</li>`
+                }).join('')
+                if(userRepos.length > 0){
+                    this.$userReposContainer.innerHTML = userRepos;
+                } else {
+                    this.$userReposContainer.innerHTML = `${user} has no repositories.`;
+                }
+                
+            },
+
+            async fetchUserFollowers(user) {
+                const userFollowersApi = await new GitHubApi();
+                const userFollowersData = await userFollowersApi.getUserFollowers(user);
+                return userFollowersData;
+            },
+
+            async updateUserFollowers (user) {
+                const userFollowersData = await this.fetchUserFollowers(user)
+                const userFollowers = await userFollowersData.map((follower) => {
+                    return `
+                            <li>
+                                <img src="${follower.avatar_url}" alt="Photo of ${follower.login}">
+                                <span>${follower.login}</span>
+                            </li>
+                            `;
+                
+                });
+                if(userFollowers.length > 0){
+                    this.$userFollowersContainer.innerHTML = userFollowers;
+                } else {
+                    this.$userFollowersContainer.innerHTML = `${user} has no followers.`;
+                }
+                
+            },
+
+            async fetchGitHubUsers (searchInput) {
+                const gitHubUsersApi = await new GitHubApi();
+                const gitHubUsers = gitHubUsersApi.getGitHubUsers(searchInput);
+                return gitHubUsers; 
+            },
+            
+            updateGitHubUsersContainer () {
+                this.$searchBar.addEventListener('input', async () => {
                     console.log(this.$searchBar.value);
+
+                    const searchData = await this.fetchGitHubUsers(this.$searchBar.value);
+                    console.log(searchData.items)
+                    const searchedUsers = await searchData.items.map((user) => {
+                        return `
+                            <li id="${user.login}">
+                                <img src="${user.avatar_url}" alt="Photo of ${user.login}">
+                                <span>${user.login}</span>
+                            </li>
+                            `;
+                    });
+
+                    this.$gitHubUsersContainer.innerHTML = searchedUsers;
+                    console.log(searchedUsers)
+                    searchData.items.forEach(user => {
+                        const $user = document.querySelector(`#${user.login}`);
+                        console.log($user)
+                        $user.addEventListener('click', () => {
+                            this.updateGitHubUserDetail(user);
+                            this.updateUserRepos(user.login);
+                            this.updateUserFollowers(user.login);
+                        } )
+                    });        
                 })
+            },
+
+            updateGitHubUserDetail (user) {                
+                const updatedUserDetail = `
+                                        <img src="${user.avatar_url}" alt="Photo of ${user.login}">
+                                        <h2>${user.login}</h2>
+                                        `
+                this.$userDetail.innerHTML = updatedUserDetail;
             },
 
             
--- static/js/services.js
+++ static/js/services.js
@@ -1,6 +1,4 @@
 function WeatherApi () {
-    this.weatherApiKey = 'c9d40b6d9988427b969105456202312';
-    this.city = 'Ghent';
     this.WEATHER_API = `http://api.weatherapi.com/v1/current.json?key=c9d40b6d9988427b969105456202312&q=%24Ghent`;
     this.getWeatherData = async () => {
         try {
@@ -8,9 +6,8 @@ function WeatherApi () {
             const weatherData = await response.json();
             return weatherData;
         }catch(error) {
-            console.error(error)
-        }
-        ; 
+            console.error(error);
+        };         
     };
 };
 
@@ -21,13 +18,56 @@ function CovidApi () {
             const response = await fetch(this.COVID_API);
             const covidData = await response.json();
             return covidData;
-        }catch(error) {
-            console.error(error)
-        }
-        ; 
+        } catch(error) {
+            console.error(error);
+        };         
     };
 };
 
+function UsersApi () {
+    this.USERS_API = '../app/static/data/pgm.json';
+    this.getUserData = async () => {
+        try {
+            const response = await fetch(this.USERS_API);
+            const userData = await response.json();
+            return userData;
+        } catch(error) {
+            console.error(error);
+        };
+    };
+}
+
 function GitHubApi () {
-    this.GITHUBAPI = ''
+    this.getUserRepos = async (userName) => {
+        this.USERREPOS_API = `https://api.github.com/users/${userName}/repos?page=1&per_page=50`
+        try {
+            const response = await fetch(this.USERREPOS_API);
+            const userRepos = await response.json();
+            return userRepos;
+        } catch(error) {
+            console.error(error)
+        }
+    };
+
+    this.getUserFollowers = async (userName) => {
+        this.USERFOLLOWERS_API = ` https://api.github.com/users/${userName}/followers?page=1&per_page=100`;
+        try {
+            const response = await fetch(this.USERFOLLOWERS_API);
+            const userFollowers = await response.json();
+            return userFollowers;
+        } catch(error) {
+            console.error(error)
+        }
+    };
+
+    this.getGitHubUsers = async (searchInput) => {
+        this.GITHUBUSERS_API = `https://api.github.com/search/users?sort=desc&page=1&per_page=100&q=${searchInput}`;
+        try {
+            const response = await fetch(this.GITHUBUSERS_API);
+            const gitHubUsers = await response.json();
+            return gitHubUsers;
+        } catch(error) {
+            console.error(error)
+        }
+    };
 }
